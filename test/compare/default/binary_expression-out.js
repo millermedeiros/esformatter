99555d86c92c8b0fc0db1e1f32faf68d0b3b6303
millermedeiros <miller@millermedeiros.com>
2012-12-06 18:35:46 -0200

    add tests for BinaryExpression

---
+++ test/compare/default/binary_expression-out.js
@@ -0,0 +1,20 @@
+1.33 >>> 0
+foo >> 3;
+2 + 2
+3 * 1;
+3 << 1
+12 | 255
+12 ^ 255
+
+// trailing
+27 & 64;
+15 % 5
+
+// asi and ExpressionStatement
+foo > bar
+a < b
+;(a == b);
+(a === b);
+a <= b
+3.25 >= b
+
