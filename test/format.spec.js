e6c03f96bc1a3321bae16d7a4da24dd1ed4e54c9
millermedeiros <miller@millermedeiros.com>
2012-12-09 01:49:24 -0200

    remove all node_modules besides mocha from repo and monkey-patch expect.js instead of editing the source file.

--- test/format.spec.js
+++ test/format.spec.js
@@ -7,13 +7,39 @@ var expect = require('expect.js');
 var _glob = require('glob');
 var _path = require('path');
 
-
 var esformatter = require('../index');
-var _helpers = require('./helpers');
 
+var _helpers = require('./helpers');
 var readOut    = _helpers.readOut;
 var readIn     = _helpers.readIn;
 var readConfig = _helpers.readConfig;
+
+
+// ---
+
+// monkey-patch expect.js for better diffs on mocha
+// see: https://github.com/LearnBoost/expect.js/pull/34
+
+var origBe = expect.Assertion.prototype.be;
+expect.Assertion.prototype.be =
+expect.Assertion.prototype.equal = function(obj){
+    this._expected = obj;
+    origBe.call(this, obj);
+};
+
+expect.Assertion.prototype.assert = function (truth, msg, error) {
+    msg = this.flags.not ? error : msg;
+    var ok = this.flags.not ? !truth : truth;
+    if (!ok) {
+        var err = new Error(msg.call(this));
+        if ('_expected' in this) {
+            err.expected = this._expected;
+            err.actual = this.obj;
+        }
+        throw err;
+    }
+    this.and = new expect.Assertion(this.obj);
+};
 
 
 // ---
