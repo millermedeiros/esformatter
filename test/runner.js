22acc406bf23388197617589d44f2d771a3690f5
millermedeiros <miller@millermedeiros.com>
2012-12-11 18:52:09 -0200

    improve test runner to allow grep, reporter options and exit with proper code if any test fails.

--- test/runner.js
+++ test/runner.js
@@ -1,17 +1,27 @@
+"use strict";
+
 // we run mocha manually otherwise istanbul coverage won't work
 // run `npm test --coverage` to generate coverage report
 
 var Mocha = require('mocha');
 
+
 var m = new Mocha({
     ui : 'bdd',
-    // we use the dot reporter on travis
-    reporter : !!process.env.npm_config_dot? 'dot' : 'spec'
+    // we use the dot reporter on travis since it works better
+    reporter : (process.env.npm_config_reporter || 'spec'),
+    grep : process.env.npm_config_grep
 });
+
+if (process.env.npm_config_invert) {
+    m.invert();
+}
+
 
 m.addFile('test/format.spec.js');
 
-m.run(function(){
-    process.exit();
+m.run(function(err){
+    var exitCode = err? 1 : 0;
+    process.exit(exitCode);
 });
 
