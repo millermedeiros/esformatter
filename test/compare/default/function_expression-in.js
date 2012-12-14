7394129b0fec2def18c1a742cbe6bc1b41031c84
millermedeiros <miller@millermedeiros.com>
2012-12-14 02:07:55 -0200

    improve ReturnStatement and FunctionExpression

---
+++ test/compare/default/function_expression-in.js
@@ -0,0 +1,23 @@
+var a=function(  ){ return 'b';};
+
+b = function doB(q,wer  , ty   ) {
+var c =         function(n) {
+    return      function(){ return q +
+            wer - ty; }
+}
+    return c}
+
+this.foo = {
+    bar : function(){
+        var r = function(){
+            re(); draw();
+            return log('foo') + 'bar';};},
+    ipsum : function ( amet ) {
+        return function( ) { amet() }
+    }
+};
+
+var noop = function (   ) {
+
+};
+
