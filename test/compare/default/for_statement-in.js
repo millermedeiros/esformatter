d478396a6756d190192277784d8f91714ef20e0f
millermedeiros <miller@millermedeiros.com>
2012-12-15 21:07:18 -0200

    improve contextual child indent rules (while/for/if/ternary)

--- test/compare/default/for_statement-in.js
+++ test/compare/default/for_statement-in.js
@@ -26,3 +26,8 @@ for
         things[i];
                 }
 
+
+// 7 : indent + no braces
+function foo() {
+for(var c=this._bindings.length,d;c--;)if (d = this._bindings[c], d._listener === a && d.context === b) return c;return -1}
+
--- test/compare/default/for_statement-out.js
+++ test/compare/default/for_statement-out.js
@@ -25,3 +25,11 @@ for (;;) {
     things[i];
 }
 
+
+// 7 : indent + no braces
+function foo() {
+    for (var c = this._bindings.length, d; c--; )
+        if (d = this._bindings[c], d._listener === a && d.context === b) return c;
+    return -1
+}
+
