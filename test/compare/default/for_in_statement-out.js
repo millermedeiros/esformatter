e8d49490df012cbc90351b46e55c947b661d8246
millermedeiros <miller@millermedeiros.com>
2012-12-17 19:35:44 -0200

    ForInStatement support

---
+++ test/compare/default/for_in_statement-out.js
@@ -0,0 +1,15 @@
+for (key in obj) {
+    doFoo(obj[key]);
+}
+for (key in obj) doFoo(obj[key]);
+
+for (var k in o) {
+    console.log(k, o[k]);
+}
+
+for (key in obj) {
+    for (prop in obj[key]) {
+        //indent
+        console.log(prop)
+    }
+}
