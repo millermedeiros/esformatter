0e5b5690bc98a7186befcf27e621c37fab2ad14d
millermedeiros <miller@millermedeiros.com>
2013-03-05 19:27:44 -0300

    add try/catch support. closes #9

---
+++ test/compare/default/try_statement-out.js
@@ -0,0 +1,21 @@
+try {
+    foo()
+} catch (e) {
+    log(e)
+}
+
+try {
+    // foo comment
+    foo();
+} finally {
+    // bar comment
+    bar();
+}
+
+try {
+    foo()
+} catch (e) {
+    log(e)
+} finally {
+    bar()
+}
