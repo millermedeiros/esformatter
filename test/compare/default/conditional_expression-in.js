2368ca3e8c1511f4692b35896e90046566f539ed
millermedeiros <miller@millermedeiros.com>
2013-03-15 16:26:36 -0300

    add failing spec for issue #6

--- test/compare/default/conditional_expression-in.js
+++ test/compare/default/conditional_expression-in.js
@@ -12,3 +12,17 @@ c = !a ?(!foo?d  :   function(){
 // should break lines
 foo.a = true; a?foo() : bar()
 
+
+// from jquery
+jQuery.prototype = {
+    get: function( num ) {
+        return num == null ?
+
+            // Return a 'clean' array
+            this.toArray() :
+
+            // Return just the object
+            ( num < 0 ? this[ this.length + num ] : this[ num ] );
+    }
+};
+
--- test/compare/default/conditional_expression-out.js
+++ test/compare/default/conditional_expression-out.js
@@ -12,3 +12,17 @@ if (true) {
 foo.a = true;
 a ? foo() : bar()
 
+
+// from jquery
+jQuery.prototype = {
+    get : function(num) {
+        return num == null ?
+
+            // Return a 'clean' array
+            this.toArray() :
+
+            // Return just the object
+            (num < 0 ? this[this.length + num] : this[num]);
+    }
+};
+
