fcd177a9c921dec87afb38aa3e35d21af6e050ce
Ruy Adorno <ruyadorno@gmail.com>
2013-06-26 23:46:13 -0300

    Initial implementation of indentation for broken lines inside if statements, fixes #32

--- test/compare/default/if_statement-in.js
+++ test/compare/default/if_statement-in.js
@@ -32,3 +32,15 @@ function iss7(){
         return;
     }
 }
+
+// issue #32
+if( foo===bar &&
+    foo>bar ){
+    foo = bar;
+}
+(function(){
+    if( foo===bar &&
+        foo>bar ){
+        foo = bar;
+    }
+})();
