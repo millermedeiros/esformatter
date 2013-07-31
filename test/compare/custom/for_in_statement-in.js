73ab9b328c654a49a0fe77da96be0a20f25300c5
Jörn Zaefferer <joern.zaefferer@gmail.com>
2013-07-31 11:03:28 +0200

    Allow more customization on for-in-statement whitespace.
    Replace ForInStatementExpression with separate ForInStatementExpressionOpening
    and ForInStatementExpressionClosing to allow whitespace insertion inside the
    statement.

---
+++ test/compare/custom/for_in_statement-in.js
@@ -0,0 +1,22 @@
+for(key in obj){doFoo(obj[key]);}
+for(    key
+    in   obj    )doFoo(obj[key]);
+
+for(var k    in    o){
+console.log(k, o[k]);
+}
+
+for(key in obj  ){
+for(prop in obj[key]) {
+//indent
+console.log(prop)
+}
+}
+
+// issue #13 : ForInStatement should not mess with inline object indent
+function iss13() {
+    for (i in {submit : true, change : true, focusin : true}) {
+      console.log(i);
+    }
+}
+
