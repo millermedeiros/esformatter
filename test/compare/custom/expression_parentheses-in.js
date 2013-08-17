596a3c68af8261c441b4c6928e34b48159a4f7be
millermedeiros <miller@millermedeiros.com>
2013-08-17 12:18:40 -0300

    Add support for Expression Parentheses. closes #60

---
+++ test/compare/custom/expression_parentheses-in.js
@@ -0,0 +1,26 @@
+;(++n<10);
+;   (a==b);
+(  c  ===  d  );
+x = (value / 10);
+if (r * (x+1)+(y+2)) {
+    y = ("123"+"3456");
+    // parseInt() is the "catch" on this case
+    z = ((q + w) / (parseInt('abc', 16) * (7) ))
+}
+
+
+// edge cases
+// ==========
+
+// not a bynary expression or a ExpressionStatement
+// VariableDeclaration > VariableDeclarator > [Identifier + Literal]
+var foo = (123);
+
+
+// SequenceExpression
+madness = (weird,stuff),(45,56)
+
+;(bar());
+(function(){})();
+(function(){}());
+
