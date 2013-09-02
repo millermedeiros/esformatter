541e8f196ff301b667d435e1a2d003eb80298717
millermedeiros <miller@millermedeiros.com>
2013-09-02 20:25:50 -0300

    start indent refactor, still many failing tests.
    looks like most hooks are naive and will need adjustments.

---
+++ test/compare/default/indent_refactor-in.js
@@ -0,0 +1,7 @@
+b = function doB(q,wer  , ty   ) {
+var c =         function(n) {
+    return      function(){ return q +
+            wer - ty; }
+}
+    return c}
+
---
+++ test/compare/default/indent_refactor-out.js
@@ -0,0 +1,9 @@
+b = function doB(q, wer, ty) {
+    var c = function(n) {
+        return function() {
+            return q + wer - ty;
+        }
+    }
+    return c
+}
+
