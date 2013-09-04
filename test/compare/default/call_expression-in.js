ea5e3ae5ddf397c93b0cb22558b69c8daca70eb3
Jörn Zaefferer <joern.zaefferer@gmail.com>
2013-09-04 13:49:37 +0200

    Test and fix for comment indentation on chained calls

--- test/compare/default/call_expression-in.js
+++ test/compare/default/call_expression-in.js
@@ -11,17 +11,21 @@ c,d)
 
 // it should indent chained calls if there is a line break between each call
 foo.bar()
+                // comment
                 .ipsum()
 .dolor();
 
 function foo() {
 dolor
+    // comment
     .amet()
                 .maecennas();
 }
 
 returned.promise().done(foo)
+// comment
 .done(newDefer.resolve)
 .fail(newDefer.reject)
+// comment
 .progress(newDefer.notify);
 
