bf25669f07a3314533771204a0758edfd0c7731e
millermedeiros <miller@millermedeiros.com>
2013-11-07 17:00:18 -0200

    make sure cli outputs descriptive error messages.

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -44,7 +44,10 @@ describe('Command line interface', function() {
       });
       childprocess.on('exit', function() {
         if (errorInChildProcess) {
-          mochaCallback(new Error(errorInChildProcess));
+          testCallback(new Error(errorInChildProcess));
+          // we don't check for the error directly because sometimes we want
+          // to test if the error is what we expect
+          mochaCallback();
         } else {
           try {
             // There is an extra line feed from piping stdout
@@ -129,6 +132,17 @@ describe('Command line interface', function() {
   filePath = path.join(__dirname + '/compare/rc/package/nested/pkg_nested-in.js');
   spawnEsformatter('nested package+rc', filePath, function(formattedFile) {
     expect(formattedFile).to.equal(helpers.readOut('/rc/package/nested/pkg_nested'));
+  });
+
+  // make sure it shows descriptive error message when config doesn't exist
+  filePath = path.join(__dirname + '/compare/default/call_expression-in.js');
+  spawnEsformatter('invalid config', '-c non-existent.json '+ filePath, function(formattedFile) {
+    expect(formattedFile.message).to.equal('Can\'t parse configuration file: "non-existent.json"\nException: ENOENT, no such file or directory \'non-existent.json\'\n');
+  });
+
+  // make sure it shows descriptive error message when file doesn't exist
+  spawnEsformatter('invalid file', 'fake-esformatter-123.js', function(formattedFile) {
+    expect(formattedFile.message).to.equal('Can\'t read source file: "fake-esformatter-123.js"\nException: ENOENT, no such file or directory \'fake-esformatter-123.js\'\n');
   });
 
 
