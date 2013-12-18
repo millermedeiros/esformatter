millermedeiros/esformatter

commit 51dccc9f2d50556d49b1f512ab7caf6b3dc0a978

fix space around ConditionalExpression parenthesis. see #119

lib/hooks/ConditionalExpression.js
@@ -3,6 +3,8 @@
 var _tk = require('rocambole-token');
 var _ws = require('../whiteSpace/whiteSpace');
 
+var addSpaceInsideExpressionParentheses = require('./addSpaceInsideExpressionParentheses');
+
 
 module.exports = function ConditionalExpression(node) {
   // we need to grab the actual punctuators since parenthesis aren't counted
@@ -17,4 +19,6 @@ module.exports = function ConditionalExpression(node) {
   _ws.afterIfNeeded(questionMark, _ws.needsBefore('ConditionalExpressionConsequent'));
   _ws.beforeIfNeeded(colon, _ws.needsAfter('ConditionalExpressionConsequent'));
   _ws.afterIfNeeded(colon, _ws.needsBefore('ConditionalExpressionAlternate'));
+
+  addSpaceInsideExpressionParentheses(node);
 };

commit 70806c07343468b38d11ee63571401c1efac57a7

ArgumentListObjectExpression: Extend jquery-preset test
Tests specifically for a regression likely caused by 51dccc9

test/compare/jquery/spacing-in.js
@@ -8,3 +8,11 @@ if (condition) { doSomething(); } else if (otherCondition) {
 } else {
 otherThing();
 }
+
+this.element
+  .add()
+  .set({
+    // line comment
+    // one more
+    prop: "value"
+  });

test/compare/jquery/spacing-out.js
@@ -10,3 +10,11 @@ if ( condition ) {
 } else {
 	otherThing();
 }
+
+this.element
+	.add()
+	.set({
+		// line comment
+		// one more
+		prop: "value"
+	});