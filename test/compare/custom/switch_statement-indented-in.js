0cf48a67ebcd18560fa3c2f4e61907f53771a4c2
Bryan Ross <bryan@rossipedia.com>
2014-05-20 12:35:52 -0600

    switch statements don't indent correctly

---
+++ test/compare/custom/switch_statement-indented-in.js
@@ -0,0 +1,23 @@
+switch ( event.keyCode ) {
+    case $.ui.keyCode.ENTER:
+        case $.ui.keyCode.SPACE:
+        // line comment
+        z();
+        break;
+    case $.ui.keyCode.ESCAPE:
+                y();
+            break;
+    default:
+    x();
+}
+
+call(function(){
+    switch (fruit) {
+        case Fruit.APPLE:
+            // line comment
+            exotic();
+            break;
+                default:
+                unknown();
+    }
+});
---
+++ test/compare/custom/switch_statement-indented-out.js
@@ -0,0 +1,25 @@
+switch ( event.keyCode )
+{
+  case $.ui.keyCode.ENTER:
+  case $.ui.keyCode.SPACE:
+    // line comment
+    z();
+    break;
+  case $.ui.keyCode.ESCAPE:
+    y();
+    break;
+  default:
+    x();
+}
+
+call(function() {
+  switch ( fruit )
+  {
+    case Fruit.APPLE:
+      // line comment
+      exotic();
+      break;
+    default:
+      unknown();
+  }
+});
