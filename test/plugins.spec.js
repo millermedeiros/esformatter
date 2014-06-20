2eb6e9b587838bf90066865178b6c66887265fed
Miller Medeiros <miller@millermedeiros.com>
2014-06-20 18:49:51 -0300

    flatten folder structure and expose more methods for plugin authors. closes #177

--- test/plugins.spec.js
+++ test/plugins.spec.js
@@ -103,6 +103,37 @@ describe('plugin API', function() {
 
   });
 
+
+  describe('exposed API', function() {
+    // plugins might need to access some internal methods from esformatter
+    // so we check if these methods are really available
+    var indent = require('../lib/indent');
+    var limit = require('../lib/limit');
+    var options = require('../lib/options');
+    var br = require('../lib/lineBreak');
+    var ws = require('../lib/whiteSpace');
+
+    it('shoud expose useful methods to plugin authors', function() {
+      // we don't need to check implementation here since these methods are
+      // used internally
+      expect(indent.indentInBetween).to.be.a('function');
+      expect(indent.indentBefore).to.be.a('function');
+      expect(limit.before).to.be.a('function');
+      expect(limit.after).to.be.a('function');
+      expect(limit.around).to.be.a('function');
+      expect(br.limit).to.be.a('function');
+      expect(br.limitBefore).to.be.a('function');
+      expect(br.limitAfter).to.be.a('function');
+      expect(ws.limit).to.be.a('function');
+      expect(ws.limitBefore).to.be.a('function');
+      expect(ws.limitAfter).to.be.a('function');
+      expect(options.set).to.be.a('function');
+      expect(options.get).to.be.a('function');
+      expect(options.getRc).to.be.a('function');
+      expect(options.loadAndParseConfig).to.be.a('function');
+    });
+  });
+
 });
 
 
