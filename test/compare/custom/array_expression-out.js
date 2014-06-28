400699b3af619f4841bb62eaa23eb184354bbd7b
Miller Medeiros <miller@millermedeiros.com>
2014-06-28 14:47:11 -0300

    add options to control line breaks inside ArrayExpressions. closes #129

--- test/compare/custom/array_expression-out.js
+++ test/compare/custom/array_expression-out.js
@@ -1,20 +1,20 @@
 [];
 
-[ 1, 2, 3 ];
+[ 1 ,2 ,3 ];
 
-[ 1, 2, [ 3, 4, [ 5, 6, [ 7, 8, 9 ] ] ] ];
+[ 1 ,2 ,[ 3 ,4 ,[ 5 ,6 ,[ 7 ,8 ,9 ] ] ] ];
 
 function fn() {
-  return [ 4, 5, [ 6, 7, 8 ] ];
+  return [ 4 ,5 ,[ 6 ,7 ,8 ] ];
 }
 
 // issue #12
 var tuples = [
   // comment test
-  [ "resolve", "done", "bla", "resolved" ],
-  [ "reject", "fail", "lorem", "rejected" ],
+  [ "resolve" ,"done" ,"bla" ,"resolved" ] ,
+  [ "reject" ,"fail" ,"lorem" ,"rejected" ] ,
   [
-    [ "lorem", "ipsum" ]
-  ],
-  [ "notify", "progress", "ipsum" ]
+    [ "lorem" ,"ipsum" ]
+  ] ,
+  [ "notify" ,"progress" ,"ipsum" ]
 ];
