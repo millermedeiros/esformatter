9376bacf537ac4c64eff82ac213b9d94f19d421a
Miller Medeiros <miller@millermedeiros.com>
2014-09-08 17:01:15 -0300

    fix indent of nested if braces before comments. closes #197

--- test/compare/default/if_statement-out.js
+++ test/compare/default/if_statement-out.js
@@ -104,3 +104,37 @@ function test(a) {
     a.two();
   }
 }
+
+
+// issue #197
+function iss197() {
+  for (var key in pattern) {
+
+    // Ignore some node properties
+    if (foo) {
+      continue;
+    }
+
+    // Match array property
+    if (_.isArray(pattern[key])) {
+      if (!bar) {
+        return false;
+      }
+
+    // Match object property
+    } else if (dolor) {
+
+      // Special case rest params (requires knowledge of sibling nodes)
+      if (ipsum) {
+        return ipsum;
+      } else if (!amet) {
+        return false;
+      }
+
+    // Match other properties (string, boolean, null, etc.)
+    } else if (pattern[key] !== node[key]) {
+      return false;
+    }
+  }
+  return true;
+}
