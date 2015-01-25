74b6fc4ac5303067d0333a263c74077d77d80f07
Jörn Zaefferer <joern.zaefferer@gmail.com>
2015-01-25 11:46:36 +0100

    FunctionExpression: Handle two special cases for TopLevelFunctionBlock
    node-style module export assignments apply, UMD wrappers don't. As the
    comment indicates, this should really be a plugin, but since the option
    is here, I'm starting with making it work better.

--- test/compare/custom/top-level-indent-exception-out.js
+++ test/compare/custom/top-level-indent-exception-out.js
@@ -13,3 +13,28 @@ setTimeout(function() {
 var x = {
   abc: 123
 };
+
+module.exports = function() {
+
+var x = 123;
+
+};
+
+(function(factory) {
+  if (typeof define === "function" && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([
+      "jquery",
+      "./core",
+      "./widget"
+    ], factory);
+  } else {
+    // Browser globals
+    factory(jQuery);
+  }
+}(function($) {
+return $.widget("ui.accordion", {
+  version: "@VERSION",
+  options: {}
+});
+}));
