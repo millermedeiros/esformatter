906ca6cceda553861bb0f90ad2eb2fe0f95c68c8
Miller Medeiros <miller@millermedeiros.com>
2015-03-30 22:11:35 -0300

    use espree to parse the programs to enable JSX support and more ES6 features. also allows the user to replace the parser if needed.

--- test/format.spec.js
+++ test/format.spec.js
@@ -2,7 +2,7 @@
 /*global describe:false, it:false*/
 "use strict";
 
-var esprima = require('esprima');
+var espree = require('espree');
 var expect = require('chai').expect;
 var _glob = require('glob');
 var _path = require('path');
@@ -42,7 +42,7 @@ describe('esformatter.format()', function() {
         // result should be valid JS
         expect(function() {
           try {
-            esprima.parse(result);
+            espree.parse(result, esformatter.format.parseOptions);
           } catch (e) {
             throw new Error('esformatter.format() result produced a non-valid output.\n' + e);
           }
@@ -78,7 +78,7 @@ describe('esformatter.format()', function() {
         // result should be valid JS
         expect(function() {
           try {
-            esprima.parse(result);
+            espree.parse(result, esformatter.format.parseOptions);
           } catch (e) {
             throw new Error('esformatter.format() result produced a non-valid output.\n' + e);
           }
@@ -113,7 +113,7 @@ describe('esformatter.format()', function() {
         // result should be valid JS
         expect(function() {
           try {
-            esprima.parse(result);
+            espree.parse(result, esformatter.format.parseOptions);
           } catch (e) {
             throw new Error('esformatter.format() result produced a non-valid output.\n' + e);
           }
