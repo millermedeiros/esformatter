77055299ad47978f1c71a0f3664b96e5af2ccb4c
Miller Medeiros <miller@millermedeiros.com>
2015-08-03 13:27:26 -0300

    limit the amount of white spaces and line breaks on MemberExpression. fixes #343

---
+++ test/compare/default/member_expression-in.js
@@ -0,0 +1,13 @@
+obj[ prop ] = foo . bar;
+
+BarNamespace [ 'Bar' ] . Bar [ "Bar" ] ( 'bar' ) . Bar ( "bar", "bar" )
+
+// should remove these line breaks
+foo.
+  bar.
+  ipum();
+
+// should NOT remove line breaks in this case
+lorem
+  .ipsum
+  .dolor();
---
+++ test/compare/default/member_expression-out.js
@@ -0,0 +1,11 @@
+obj[prop] = foo.bar;
+
+BarNamespace['Bar'].Bar["Bar"]('bar').Bar("bar", "bar")
+
+// should remove these line breaks
+foo.bar.ipum();
+
+// should NOT remove line breaks in this case
+lorem
+  .ipsum
+  .dolor();
