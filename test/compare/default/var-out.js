a8ab24a136e42a531171504666712cd1e46c5eb8
Miller Medeiros <miller@millermedeiros.com>
2016-02-13 22:43:50 -0800

    re-enable VariableDeclarationWithoutInit. fixes #382

--- test/compare/default/var-out.js
+++ test/compare/default/var-out.js
@@ -70,3 +70,14 @@ var b = '3';
 var foo = 123,
   bar = 456;
 [1, 2, 3].forEach(echo)
+
+// issue #382
+function getInverseAnswer() {
+  var a = 10,
+    b = 4,
+    answer,
+    inverse;
+  answer = a * b + (a - b - b);
+  inverse = answer * -1;
+  return inverse;
+}
