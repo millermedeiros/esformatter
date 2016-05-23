3c35e7af47c7c40b0bff9146b04a5492a2f266a2
Miller Medeiros <miller@millermedeiros.com>
2016-05-23 01:01:10 -0700

    handle standalone BlockStatement. closes #430

---
+++ test/compare/default/block_statement-out.js
@@ -0,0 +1,21 @@
+{
+  var foo = 123;
+  bar(foo)
+}
+
+function dolor() {
+  {
+    let amet = 'maecennas';
+    console.log(amet)
+  }
+}
+
+// #430
+function a() {
+  var arr2 = []
+}
+var x = 1
+{
+  var x = 2
+  console.log(x)
+}
