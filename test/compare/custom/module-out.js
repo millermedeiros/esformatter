1cad7ea1b23bb13ec8eec9c5ec40fc12589f0873
Miller Medeiros <miller@millermedeiros.com>
2016-07-14 23:30:40 -0700

    add configs for ModuleSpecifier{Opening,Closing}Brace and ModuleSpecifierComma. closes #402

--- test/compare/custom/module-out.js
+++ test/compare/custom/module-out.js
@@ -1,12 +1,23 @@
 // samples borrowed from http://www.2ality.com/2014/09/es6-modules-final.html
 
 // Default exports and named exports
-import theDefault, {   named1, named2   } from 'src/mylib';
+import theDefault, {
+  named1,
+  named2
+} from 'src/mylib';
+
 import theDefault from 'src/mylib';
-import {   named1, named2   } from 'src/mylib';
+
+import {
+  named1,
+  named2
+} from 'src/mylib';
 
 // Renaming: import named1 as myNamed1
-import {   named1 as myNamed1, named2   } from 'src/mylib';
+import {
+  named1 as myNamed1,
+  named2
+} from 'src/mylib';
 
 // Importing the module as an object
 // (with one property per named export)
@@ -16,7 +27,9 @@ import * as mylib from 'src/mylib';
 import 'src/mylib';
 
 export var myVar1 = 123;
+
 export let myVar2 = 'foo';
+
 export const MY_CONST = 'BAR';
 
 export function myFunc() {
@@ -26,6 +39,7 @@ export function myFunc() {
 export function* myGeneratorFunc() {
   return yield someStuff();
 }
+
 export class MyClass {
   constructor() {
     this.name = 'exports test';
@@ -33,10 +47,13 @@ export class MyClass {
 }
 
 export default 123;
+
 export default function (x) {
   return x
 }
+
 export default x => x;
+
 export default class {
   constructor(x, y) {
     this.x = x;
@@ -44,9 +61,25 @@ export default class {
   }
 }
 
-export {   MY_CONST, myFunc   };
-export {   MY_CONST as THE_CONST, myFunc as theFunc   };
+export {
+  MY_CONST,
+  myFunc
+};
+
+export {
+  MY_CONST as THE_CONST,
+  myFunc as theFunc
+};
 
 export * from 'src/other_module';
-export {   foo, bar   } from 'src/other_module';
-export {   foo as myFoo, bar   } from 'src/other_module';
+
+export {
+  foo,
+  bar
+} from 'src/other_module';
+
+export {
+  foo as myFoo,
+  bar
+} from 'src/other_module';
+
